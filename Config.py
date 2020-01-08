f1dd5c1bf6c4aec2eef1c2c3754aaeb21aadb16e
Alex Singleton <alexsingleton22@gmail.com>
2020-01-08 01:24:38 -0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,5 +4,5 @@ agent = 'python:(name):(version) (by (author))'
 user = 'bot username'
 password = 'bot password'
 subreddit = 'subreddit'
-permissions_api = 'api key'  # permissions feature uses a different API (AppMonsta)
+permissions_api = 'api key'  # several features use a different API (AppMonsta)
 blacklisted_devs = []
