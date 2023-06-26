c0c4ee4c91c69c9f92de885cb23b74680d727312
a-ton <alexsingleton22@gmail.com>
2023-06-26 17:36:58 -0400

    Code cleaup and rewrites, fixes to cover edge case

--- msg_monitor.py
+++ msg_monitor.py
@@ -1,10 +1,7 @@
 import time
 import praw
-import prawcore 
-import requests
+import prawcore
 import Config
-from bs4 import BeautifulSoup
-responded = 0
 footer = "\n\n*****\n\n^^^[Source](https://github.com/a-ton/gpd-bot) ^^^| ^^^[Suggestions?](https://www.reddit.com/message/compose?to=Swimmer249)"
 reddit = praw.Reddit(client_id=Config.cid,
                      client_secret=Config.secret,
@@ -16,49 +13,43 @@ print("Monitoring inbox...")
 while True:
     try:
         for msg in reddit.inbox.stream():
-            expired = False
-            oops = False
-            responded = 0
             # checks if bot has already replied (good if script has to restart)
-            try:
-                if isinstance(msg, praw.models.Comment):
-                    for comment in msg.refresh().replies:
-                        try:
-                            if comment.author.name == "GPDBot":
-                                responded = 1
-                        except AttributeError:
-                            responded = 0
-                print("Message recieved")
-            except AttributeError:
-                print("error checking comment by: " + msg.author.name)
+            if isinstance(msg, praw.models.Comment):
+                responded = False
+                for comment in msg.refresh().replies:
+                    if comment.author.name == "GPDBot":
+                        responded = True
+                        break
+                if responded:
+                    continue
             # checks if the message body contains "expired" or "oops"
-            try:
-                if responded == 0:
-                    if isinstance(msg, praw.models.Comment):
-                        text = msg.body.lower()
-                        try:
-                            if text.index("oops") > -1:
-                                oops = True
-                        except ValueError:
-                            pass
-                        try:
-                            if text.index("expired") > -1:
-                                expired = True
-                        except ValueError:
-                            pass
-                        if oops:
-                            msg.mark_read()
-                            msg.submission.mod.flair(text=None, css_class=None)
-                            print("unflairing... responded to: " + msg.author.name)
-                            msg.reply("Flair removed." + footer)
-                        elif expired:
-                            msg.mark_read()
-                            title_url = msg.submission.url
-                            msg.submission.mod.flair(text='Deal Expired', css_class='expired')
-                            print("flairing... responded to: " + msg.author.name)
-                            msg.reply("Deal marked as expired. Reply with \"oops\" if this is incorrect." + footer)
-            except AttributeError:
-                print("error checking comment by: " + msg.author.name)
+            if isinstance(msg, praw.models.Comment):
+                msg_text = msg.body.lower()
+                oops = False
+                expired = False
+                try:
+                    if msg_text.index("oops") > -1:
+                        oops = True
+                except ValueError:
+                    pass
+                try:
+                    if msg_text.index("expired") > -1:
+                        expired = True
+                except ValueError:
+                    pass
+                reply_msg = ""
+                if oops:
+                    msg.submission.mod.flair(text=None, css_class=None)
+                    print("unflairing... responded to: " + msg.author.name)
+                    reply_msg = "Flair removed." + footer
+                elif expired:
+                    msg.submission.mod.flair(text='Deal Expired', css_class='expired')
+                    print("flairing... responded to: " + msg.author.name)
+                    reply_msg = "Deal marked as expired. Reply with \"oops\" if this is incorrect." + footer
+
+                if reply_msg != "":
+                    msg.mark_read()
+                    msg.reply(body=reply_msg)
     except (prawcore.exceptions.RequestException, prawcore.exceptions.ResponseException):
         print ("Error connecting to reddit servers. Retrying in 5 minutes...")
         time.sleep(300)
--- reddit_response.py
+++ reddit_response.py
@@ -33,28 +33,28 @@ class AppInfo:
     def getName(self):
         return self.store_page.find("h1", itemprop="name").get_text()
     
-    def getNumDownloads(self):
+    def get_downloads(self):
         for item in self.expanded_details:
             if "Downloads" in item.text:
                 downloads = item.text[len("Downloads"):]
                 return downloads
         return "Couldn't get downloads"
     
-    def getRating(self):
+    def get_rating(self):
         try:
             rating = self.store_page.find("div", class_="TT9eCd").get_text()
         except AttributeError:
             return "NA    "
         return rating[:-4] + "/5"
 
-    def getDeveloper(self):
+    def get_developer(self):
         dev = self.store_page.find("div", class_="Vbfug auoIOc")
         dev_url = dev.find("a").get("href")
         if dev.get_text() in Config.blacklisted_devs:
             raise BlacklistedDev
         return "[" + dev.get_text() + "]" + "(https://play.google.com" + dev_url + ")"
 
-    def getLastUpdateDate(self):
+    def get_update_date(self):
         for item in self.expanded_details:
             if "Updated on" in item.text:
                 return item.text[len("Updated on"):]
@@ -78,32 +78,32 @@ class AppInfo:
         return sale_price + " was " + full_price
     
 
-    def getPlayPass(self):
+    def get_play_pass(self):
         play_pass = self.store_page.find("a", class_="pFise")
         if play_pass:
             return "\n**Included with Play Pass**  "
         return ""
 
-    def getAds(self):
+    def get_ads(self):
         check = self.store_page.findAll("span", class_="UIuSk")
         for item in check:
             if "Contains ads" in item.get_text():
                 return "Yes"
         return "No"
 
-    def getIAPInfo(self):
-        IAP_check = "No"
+    def get_iap_info(self):
+        iap_info = "No"
         check = self.store_page.findAll("span", class_="UIuSk")
         for item in check:
             if "In-app purchases" in item.get_text():
-                IAP_check = "Yes"
-        if IAP_check == "Yes":
+                iap_info = "Yes"
+        if iap_info == "Yes":
             for selenium_item in self.expanded_details:
                 if "In-app purchases" in selenium_item.text:
-                    IAP_check += ", " + selenium_item.text[len("In-app purchases"):]
-        return IAP_check
+                    iap_info += ", " + selenium_item.text[len("In-app purchases"):]
+        return iap_info
 
-    def getPermissions(self):
+    def get_permissions(self):
         perm_list = ""
 
         for perm in self.expanded_permissions:
@@ -117,18 +117,18 @@ class AppInfo:
 
         return "Permissions: " + perm_list + "  "
 
-    def getDescription(self):
+    def get_description(self):
         desc_strings = self.store_page.find("div", class_="bARER").stripped_strings
         desc = ''
-        totalChar = 0
-        totalLines = 0
-        for s in desc_strings:
-            desc += '>' + s + '  \n'
-            totalChar += len(s)
-            totalLines += 1
-            if totalChar >= 400:
+        total_chars = 0
+        total_lines = 0
+        for string in desc_strings:
+            desc += '>' + string + '  \n'
+            total_chars += len(string)
+            total_lines += 1
+            if total_chars >= 400:
                 break
-            if totalLines >= 10:
+            if total_lines >= 10:
                 break
         return desc
 
@@ -137,44 +137,44 @@ class AppInfo:
         self.invalid = False
         self.submission = submission
         page = requests.get(url).text
-        
+
         self.store_page = BeautifulSoup(page, "html.parser")
         self.name = self.getName()
-        self.rating = self.getRating()
+        self.rating = self.get_rating()
         try:
-            self.developer = self.getDeveloper()
+            self.developer = self.get_developer()
         except BlacklistedDev:
             self.blacklist = True
         self.price_info = self.getPriceInfo()
-        self.play_pass = self.getPlayPass()
-        self.desc = self.getDescription()
+        self.play_pass = self.get_play_pass()
+        self.description = self.get_description()
         self.url = url
-        self.ads = self.getAds()
+        self.ads = self.get_ads()
 
         self.selenium = webdriver.Firefox()
         self.selenium.get(url)
         time.sleep(5)
-        details_button = self.selenium.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div/div[1]/div[2]/div/div[1]/div[1]/c-wiz[2]/div/section/header/div/div[2]/button")
+        details_button = self.selenium.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div/div[2]/div[2]/div/div[1]/div[1]/c-wiz[2]/div/section/header/div/div[2]/button")
         details_button.click()
         time.sleep(1)
-        
+
         self.expanded_details = self.selenium.find_elements(By.CLASS_NAME, "sMUprd")
-        self.downloads = self.getNumDownloads()
-        self.last_update = self.getLastUpdateDate()
+        self.downloads = self.get_downloads()
+        self.last_update = self.get_update_date()
         #self.size = self.getSize()
-        self.IAP_info = self.getIAPInfo()
-        
+        self.iap_info = self.get_iap_info()
+
         permissions_button = self.selenium.find_element(By.CSS_SELECTOR, "span.TCqkTe")
         permissions_button.click()
         time.sleep(1)
         self.expanded_permissions = self.selenium.find_elements(By.CLASS_NAME, "aPeBBe")
-        self.permissions = self.getPermissions()
+        self.permissions = self.get_permissions()
         self.selenium.close()
-    
+
 
 def flair(app_rating, num_installs, sub):
     inst = num_installs.split("+")
-    if (inst[0] == "Couldn't"):
+    if inst[0] == "Couldn't":
         return
     try:
         val = int(inst[0].replace(',', ''))
@@ -202,106 +202,85 @@ def respond(submission):
 ^^^|
 ^^^[Suggestions?](https://www.reddit.com/message/compose?to=Swimmer249)"""
     
-    urls = []
+    all_urls = []
     if submission.is_self:
-        urls = re.findall('(?:(?:https?):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', submission.selftext)
-        if len(urls) == 0:
+        all_urls = re.findall('(?:(?:https?):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', submission.selftext)
+        if len(all_urls) == 0:
             print("NO LINK FOUND skipping: " + submission.title)
             logID(submission.id)
             return
+    else:
+        all_urls.append(submission.url)
 
     # remove duplicate URLs
-    unique_urls = []
-    for url in urls:
-        if url in unique_urls:
-            continue
-        else:
-            unique_urls.append(url)
+    unique_urls = [*set(all_urls)]
 
     # find apps that we can respond to
     valid_apps = []
+    required_url = ["http", "play.google"]
+    disallowed_url = ["collection/cluster", "dev?id="]
     for url in unique_urls:
-        if not "http" in url or not "play.google" in url or "collection/cluster" in url:
+        # check if strings in required_url are part of url and if strings in disallowed_url do not exist in url
+        if not all(x in url for x in required_url):
+            continue
+        if any(x in url for x in disallowed_url):
             continue
         app = AppInfo(submission, url)
+        if app.blacklist:
+            reply_text = "Sorry, deals from one or more of the developers in your post have been blacklisted. Here is the full list of blacklisted developers: https://www.reddit.com/r/googleplaydeals/wiki/blacklisted_devlopers"
+            submission.mod.remove()
+            submission.reply(body=reply_text).mod.distinguish()
+            print("Removed (developer blacklist): " + submission.title)
+            logID(submission.id)
+            return
         if app.invalid:
             continue
         valid_apps.append(app)
         if len(valid_apps) >= 10:
             break
 
-    if not submission.is_self:
-        trimurl = submission.url.split("&")[0]
-        if "collection/cluster" in trimurl:
-            print("SKIPPING, COLLECTION: " + submission.title)
-            logID(submission.id)
-            return
-        app = AppInfo(submission, trimurl)
-
-    if len(valid_apps) == 1:
-        app = valid_apps[0]
-
-    if submission.is_self and len(valid_apps) == 0:
+    if len(valid_apps) == 0:
         print("All invalid links, skipping: " + submission.title)
         logID(submission.id)
         return
-        
-    if len(valid_apps) > 1:
-        reply_text = ""
-        for app_num in range(len(valid_apps)):
-            if (app_num >= 10):
-                break
-            app = valid_apps[app_num]
-            if app.blacklist:
-                reply_text = "Sorry, deals from one or more of the developers in your post have been blacklisted. Here is the full list of blacklisted developers: https://www.reddit.com/r/googleplaydeals/wiki/blacklisted_devlopers"
-                submission.mod.remove()
-                submission.reply(body=reply_text).mod.distinguish()
-                print("Removed (developer blacklist): " + submission.title)
-                logID(submission.id)
-                return
-            reply_text += "Info for [%s](%s): Price (USD): %s | Rating: %s | Installs: %s | IAPs/Ads: %s/%s\n\n*****\n\n" % (app.name, app.url, app.price_info, app.rating, app.downloads, app.IAP_info, app.ads)
-        if len(valid_apps) >= 10:
-            reply_text += "...and more. Max of 10 apps reached.\n\n*****\n\n"
-        reply_text += "If any of these deals have expired, please reply to this comment with \"expired\". ^^^Abuse ^^^will ^^^result ^^^in ^^^a ^^^ban."
-        reply_text += footer
-        
-        submission.reply(body=reply_text)
-        
-        print("Replied to: " + submission.title)
-        logID(submission.id)
-        return
 
-    if not app.invalid:
+    reply_text = ""
+
+    if len(valid_apps) == 1:
         flair(app.rating, app.downloads, submission)
 
-    if app.blacklist:
-        reply_text = "Sorry, deals from one or more of the developers in your post have been blacklisted. Here is the full list of blacklisted developers: https://www.reddit.com/r/googleplaydeals/wiki/blacklisted_devlopers"
-        submission.mod.remove()
-        submission.reply(body=reply_text).mod.distinguish()
-        print("Removed (developer blacklist): " + submission.title)
-    elif app.invalid:
-        print("INCORRECT LINK Skipping: " + submission.title)
-    else:
-        reply_text = """Info for %s:
+        reply_text = f"""Info for {app.name}:
 
-Current price (USD): %s  %s
-Developer: %s  
-Rating: %s  
-Installs: %s  
-Last updated: %s  
-Contains IAPs: %s  
-Contains Ads: %s  
-%s
+Current price (USD): {app.price_info}  {app.play_pass}
+Developer: {app.developer}  
+Rating: {app.rating}  
+Installs: {app.downloads}  
+Last updated: {app.last_update}  
+Contains IAPs: {app.iap_info}  
+Contains Ads: {app.ads}  
+{app.permissions}
 Short description:  
 
-%s  
+{app.description}  
 
 ***** 
 
-If this deal has expired, please reply to this comment with \"expired\". ^^^Abuse ^^^will ^^^result ^^^in ^^^a ^^^ban.""" % (app.name, app.price_info, app.play_pass, app.developer, app.rating, app.downloads, app.last_update, app.IAP_info, app.ads, app.permissions, app.desc)
-        reply_text += footer
-        submission.reply(body=reply_text)
-        print("Replied to: " + submission.title)
+If this deal has expired, please reply to this comment with \"expired\". ^^^Abuse ^^^will ^^^result ^^^in ^^^a ^^^ban."""
+
+
+    if len(valid_apps) > 1:
+        reply_text = ""
+        for app_num, app in enumerate(valid_apps):
+            if app_num >= 10:
+                break
+            reply_text += f"Info for [{app.name}]({app.url}): Price (USD): {app.price_info} | Rating: {app.rating} | Installs: {app.downloads} | IAPs/Ads: {app.IAP_info}/{app.ads}\n\n*****\n\n"
+        if len(valid_apps) >= 10:
+            reply_text += "...and more. Max of 10 apps reached.\n\n*****\n\n"
+        reply_text += "If any of these deals have expired, please reply to this comment with \"expired\". ^^^Abuse ^^^will ^^^result ^^^in ^^^a ^^^ban."
+
+    reply_text += footer
+    submission.reply(body=reply_text)
+    print("Replied to: " + submission.title)
     logID(submission.id)
 
 while True:
